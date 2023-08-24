4e8fac521a041689de3f31403e3cd4e95fc19f2a
toimc <brain@toimc.com>
2023-08-24 23:14:22 +0800

    refactor: remove assets

--- server/api/home.get.ts
+++ server/api/home.get.ts
@@ -3,9 +3,7 @@ export default defineEventHandler(() => {
   // env prod: api -> 正常请求
   return {
     code: 200,
-    data: {
-      swipers: []
-    },
-    message: '请求成功'
+    data: {},
+    message: 'from real api'
   }
 })
