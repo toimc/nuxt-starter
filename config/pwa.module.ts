89201d9c1fa8a378d685a60d81c7db8ede8bc107
toimc <brain@toimc.com>
2024-01-21 12:02:27 +0800

    feat: pwa module

---
+++ config/pwa.module.ts
@@ -0,0 +1,60 @@
+import process from 'node:process'
+import type { ModuleOptions } from '@vite-pwa/nuxt'
+
+const scope = '/'
+const appName = 'Nuxt starter'
+const appDescription = 'Nuxt3+Vite+PWA ts starter'
+
+export const pwa: ModuleOptions = {
+  registerType: 'autoUpdate',
+  scope,
+  base: scope,
+  manifest: {
+    id: scope,
+    scope,
+    name: appName,
+    short_name: appName,
+    description: appDescription,
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'maskable-icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ]
+  },
+  workbox: {
+    globPatterns: ['**/*.*'],
+    // globPatterns: ['**/*.{js,css,html,txt,png,ico,svg}'],
+    navigateFallback: '/',
+    cleanupOutdatedCaches: true,
+    runtimeCaching: [
+      // ...
+    ]
+  },
+  client: {
+    installPrompt: true,
+    // you don't need to include this: only for testing purposes
+    // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+    periodicSyncForUpdates: 3600
+  },
+  registerWebManifestInRouteRules: true,
+  writePlugin: true,
+  devOptions: {
+    enabled: process.env.VITE_PLUGIN_PWA === 'true',
+    suppressWarnings: true,
+    navigateFallback: scope
+  }
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import { pwa } from './config/pwa.module'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
@@ -55,42 +56,5 @@ export default defineNuxtConfig({
       ]
     }
   },
-
-  pwa: {
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'Vite App',
-      short_name: 'Vite App',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: '/192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    workbox: {
-      navigateFallback: '/',
-      // 如果大家有很大的资源文件，wasm bundle.js
-      globPatterns: ['**/*.*']
-    },
-    client: {
-      installPrompt: true,
-      // you don't need to include this: only for testing purposes
-      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-      periodicSyncForUpdates: 3600
-    },
-    devOptions: {
-      enabled: true,
-      suppressWarnings: true,
-      navigateFallbackAllowlist: [/^\/$/],
-      type: 'module'
-    }
-  }
+  pwa
 })
